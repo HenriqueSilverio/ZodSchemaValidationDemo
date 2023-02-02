84ccd2dd066ac73d352bda47c1ad3d28c7f4dda5
Henrique Silverio <2057432+HenriqueSilverio@users.noreply.github.com>
2023-02-02 15:44:19 -0300

    chore: wire things up

--- src/Main.ts
+++ src/Main.ts
@@ -1,6 +1,26 @@
 import express, { Request, Response } from 'express'
 
+import RequestValidator from './RequestValidator'
+
+import AddNewTransactionController from './AddNewTransactionController'
+import AddNewTransactionSchema from './AddNewTransactionSchema'
+
+import ShowTransactionController from './ShowTransactionController'
+import ShowTransactionSchema from './ShowTransactionSchema'
+
+import ListTransactionController from './ListTransactionController'
+import ListTransactionSchema from './ListTransactionSchema'
+
 const server = express()
+
+const addNewTransactionValidator = new RequestValidator(AddNewTransactionSchema)
+const addNewTransactionController = new AddNewTransactionController()
+
+const listTransactionValidator = new RequestValidator(ListTransactionSchema)
+const listTransactionController = new ListTransactionController()
+
+const showTransactionValidator = new RequestValidator(ShowTransactionSchema)
+const showTransactionController = new ShowTransactionController()
 
 server.use(express.json())
 
@@ -11,6 +31,24 @@ server.get('/', (request: Request, response: Response): void => {
     message: "Hello, from Zod's Demo",
   })
 })
+
+server.post(
+  '/transactions',
+  addNewTransactionValidator.handler(),
+  addNewTransactionController.handler(),
+)
+
+server.get(
+  '/transactions',
+  listTransactionValidator.handler(),
+  listTransactionController.handler(),
+)
+
+server.get(
+  '/transactions/:transactionID',
+  showTransactionValidator.handler(),
+  showTransactionController.handler(),
+)
 
 server.listen(3080, () => {
   console.log('🚀 Listening at http://localhost:3080...')
